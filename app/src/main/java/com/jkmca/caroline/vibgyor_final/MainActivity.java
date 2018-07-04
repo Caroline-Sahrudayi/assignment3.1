5cfcf57ee7469292fa6647decb34d91ed67a4f79
Caroline-Sahrudayi <carolineps777@gmail.com>
2018-07-04 22:36:19 +0530

    this is the first commit

---
+++ app/src/main/java/com/jkmca/caroline/vibgyor_final/MainActivity.java
@@ -0,0 +1,19 @@
+package com.jkmca.caroline.vibgyor_final;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    LinearLayout linearLayout;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+     linearLayout=(LinearLayout)findViewById(R.id.activity_main);
+    }
+
+}
